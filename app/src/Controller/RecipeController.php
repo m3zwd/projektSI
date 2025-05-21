f79ae5c76cee481f579cf654021cfc725406b97a
Martyna Zawada <martyna.zawada@host.com>
2025-05-21 18:13:29 +0200

    Dodanie kontrolera dla przepisów, dodanie szablonów: wyświetlania listy rekordów i pojedynczego rekordu, dodanie tłumaczeń, instalacja paginatora

---
+++ app/src/Controller/RecipeController.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Recipe controller.
+ */
+
+namespace App\Controller;
+
+use App\Entity\Recipe;
+use App\Repository\RecipeRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Class RecipeController.
+ */
+#[Route('/recipe')]
+class RecipeController extends AbstractController
+{
+    /**
+     * Index action.
+     *
+     * @param RecipeRepository   $recipeRepository Recipe repository
+     * @param PaginatorInterface $paginator        Paginator
+     * @param int                $page             Default page number
+     *
+     * @return Response HTTP response
+     */
+    #[Route(
+        name: 'recipe_index',
+        methods: 'GET'
+    )]
+    public function index(RecipeRepository $recipeRepository, PaginatorInterface $paginator, #[MapQueryParameter] int $page = 1): Response
+    {
+        $pagination = $paginator->paginate(
+            $recipeRepository->queryAll(),
+            $page,
+            RecipeRepository::PAGINATOR_ITEMS_PER_PAGE,
+            [
+                'sortFieldAllowList' => ['recipe.id', 'recipe.createdAt', 'recipe.updatedAt', 'recipe.title'],
+                'defaultSortFieldName' => 'recipe.updatedAt',
+                'defaultSortDirection' => 'desc',
+            ]
+        );
+
+        return $this->render('recipe/index.html.twig', ['pagination' => $pagination]);
+    }
+
+    /**
+     * View action.
+     *
+     * @param Recipe $recipe Recipe entity
+     *
+     * @return Response HTTP response
+     */
+    #[Route(
+        '/{id}',
+        name: 'recipe_view',
+        requirements: ['id' => '[1-9]\d*'],
+        methods: 'GET'
+    )]
+    public function view(Recipe $recipe): Response
+    {
+        return $this->render(
+            'recipe/view.html.twig',
+            ['recipe' => $recipe]
+        );
+    }
+}
--- app/src/Repository/RecipeRepository.php
+++ app/src/Repository/RecipeRepository.php
@@ -1,43 +1,51 @@
 <?php
 
+/**
+ * Recipe repository.
+ */
+
 namespace App\Repository;
 
 use App\Entity\Recipe;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
+ * Class Recipe Repository.
+ *
  * @extends ServiceEntityRepository<Recipe>
  */
 class RecipeRepository extends ServiceEntityRepository
 {
+    /**
+     * Items per page.
+     *
+     * Use constants to define configuration options that rarely change instead
+     * of specifying them in configuration files.
+     * See https://symfony.com/doc/current/best_practices.html#configuration
+     *
+     * @constant int
+     */
+    public const PAGINATOR_ITEMS_PER_PAGE = 10;
+
+    /**
+     * Constructor.
+     *
+     * @param ManagerRegistry $registry
+     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Recipe::class);
     }
 
-    //    /**
-    //     * @return Recipe[] Returns an array of Recipe objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('r.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Recipe
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    /**
+     * Query all records.
+     *
+     * @return QueryBuilder Query Builder
+     */
+    public function queryAll(): QueryBuilder
+    {
+        return $this->createQueryBuilder('recipe');
+    }
 }
